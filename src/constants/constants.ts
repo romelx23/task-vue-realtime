8591fabffb49117982b6f13f4b71eee8e41d23e5
romelx23 <romelx23@gmail.com>
2024-03-28 16:24:01 -0500

    update server url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const urlServer =
-  import.meta.env.VITE_URL_SERVER || "http://localhost:8080";
+export const urlServer = "https://task-back-socket.onrender.com";
+// import.meta.env.VITE_URL_SERVER || "http://localhost:8080";
